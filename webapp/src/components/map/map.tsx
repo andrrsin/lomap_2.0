5977cd399653702ee039256f5e7d3d8b05c0e901
Andrés Cadenas <andrescabla@gmail.com>
2023-06-13 21:18:25 +0200

    different tests with the gmaps api

--- webapp/src/components/map/map.tsx
+++ webapp/src/components/map/map.tsx
@@ -6,9 +6,9 @@ import { useEffect } from 'react';
 export const Map = () => {
     const { isLoaded } = useJsApiLoader({ id: 'google-map-script', googleMapsApiKey: "AIzaSyAm4-Y9DXFycCPlBGSfENndiTKtmBKz-GQ" })
     //save map in ref
-    const mapRef = React.useRef<google.maps.Map | null>(null);
+    const mapRef = React.useRef<google.maps.Map>(new google.maps.Map(document.createElement('div')));
     const onLoad = (map: google.maps.Map): void => { mapRef.current = map };
-    const unMount = (): void => { mapRef.current = null };
+    const unMount = (): void => { mapRef.current = new google.maps.Map(document.createElement('div')) };
     const [origin,setOrigin] = useState<google.maps.LatLngLiteral>({ lat: 0, lng: 0 });
     useEffect(() => {
         // get the user's current location
@@ -27,13 +27,22 @@ export const Map = () => {
         map:mapRef.current,
     }
     let marker = new google.maps.Marker(markerOptions);
-
+    marker.addListener("click", (e:google.maps.MapMouseEvent) => {
+        mapRef.current.setZoom(8);
+        mapRef.current.setCenter(marker.getPosition() as google.maps.LatLng);
+      });
+    mapRef.current.addListener("click", (e:google.maps.MapMouseEvent) => {
+        document.getElementById("lat")!.innerHTML = e.latLng?.lat().toString()||"";
+      });
+      
     if (!isLoaded) return <div>Loading...</div>;
     return (
 
         <div className="mapWrapper">
             <GoogleMap mapContainerStyle={containerStyle} options={options as google.maps.MapOptions} center={origin} zoom={13} onLoad={onLoad} onUnmount={unMount} />
-            
+            <div className="lat">
+
+            </div>
         </div>
     );
 
