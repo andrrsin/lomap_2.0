4188566270f9fb7ab61602fc4333a08c6e018bbf
Andrés Cadenas <andrescabla@gmail.com>
2023-07-04 03:42:33 +0200

    Update logout.steps.ts

--- webapp/e2e/steps/logout.steps.ts
+++ webapp/e2e/steps/logout.steps.ts
@@ -1,7 +1,7 @@
 import { defineFeature, loadFeature } from "jest-cucumber";
 import puppeteer from "puppeteer";
 
-const feature = loadFeature('./features/viewProfile.feature');
+const feature = loadFeature('./features/logout.feature');
 
 let page: puppeteer.Page;
 let browser: puppeteer.Browser;
